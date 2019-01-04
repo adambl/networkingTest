211080e6ae2cd8880cae894db749d9d187cede42
adam.blaiberg <adam.blaiberg@appsflyer.com>
2019-01-04 09:23:26 +0200

    Calculate blast radius and building health decreases in opposite relation to the distance from the missle collision. Problem currently: Cannot destroy building not owned by me.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,15 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
-    GameObject missleLauncherPrefab;
     Vector3 masterPosition = new Vector3(0f, 0.5f, -30f);
-    Vector3 secondPlayerPosition = new Vector3(0f, 0.5f, 30f); 
+    Vector3 secondPlayerPosition = new Vector3(0f, 0.5f, 30f);
 
+    DamageManager damageManager;
     public void Start()
     {
         Debug.Log(string.Format("In GameManager. IsMasterClient: {0}", PhotonNetwork.IsMasterClient));
+
+        PhotonNetwork.CurrentRoom.SetCustomProperties(new Hashtable() { { "Winner", null } });
 
         Vector3 launcherPosition = masterPosition;
         if (!PhotonNetwork.IsMasterClient)
@@ -24,10 +26,16 @@ public class GameManager : MonoBehaviourPunCallbacks
             "MissleLauncher", 
             launcherPosition, 
             Quaternion.identity);
-    }
 
-    [Tooltip("The prefab to use for representing the player")]
-    public GameObject playerPrefab;
+        GameObject building = PhotonNetwork.Instantiate(
+            "Building",
+            launcherPosition + new Vector3(3,0,0),
+            Quaternion.identity);
+
+        damageManager = (DamageManager)GameObject.Find("DamageManager").GetComponent<DamageManager>();
+        //TODO - count adversary buildings and not your own. currently it is the same.
+        damageManager.NumOfBuildings = 1;
+    }
 
     #region Photon Callbacks
     public override void OnPlayerLeftRoom(Player other)
@@ -52,7 +60,8 @@ public class GameManager : MonoBehaviourPunCallbacks
     public void OnPhotonCustomRoomPropertiesChanged(Hashtable propertiesThatChanged)
     {
         Debug.LogFormat("OnPhotonCustomRoomPropertiesChanged() {0}", propertiesThatChanged.ToString()); // custom room properties changed
-        //Todo - respond to loose indication
+        //TODO - respond to win indication
+
     }
 
     #endregion
