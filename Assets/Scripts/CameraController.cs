3cf057726f7129dacce29e4d6f80493e2bb16dbf
adam.blaiberg <adam.blaiberg@appsflyer.com>
2019-01-04 09:30:48 +0200

    Added untracked necessary files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CameraController : MonoBehaviour
+{
+    Quaternion cameraRotation = Quaternion.Euler(175f, 0, 180f);
+    private float cameraZ = 36f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            transform.position = new Vector3(0, 6, cameraZ);
+            transform.rotation = cameraRotation;
+        }
+    }
+
+}
